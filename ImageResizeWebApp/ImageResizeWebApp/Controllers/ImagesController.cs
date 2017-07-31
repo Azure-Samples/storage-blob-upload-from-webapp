5fc96621fe3b0094ac647bd9e1227399113eb738
Sowmyan Soman <sowmyans@outlook.com>
2017-07-31 17:30:07 -0400

    configured the connectionstring retriveal model from appsettings.js

---
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ImageResizeWebApp.Models;
+using Microsoft.Extensions.Options;
+
+namespace ImageResizeWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ImagesController : Controller
+    {
+        private readonly AzureStorageConfig _storageConfig;
+
+        public ImagesController(IOptions<AzureStorageConfig> config)
+        {
+            _storageConfig = config.Value;
+        }
+
+        [HttpGet("[action]")]
+        public string Storage()
+        {
+            var val = _storageConfig;
+            return "https://url";
+        }
+
+        [HttpGet]
+        public string List()
+        {
+            var val = _storageConfig;
+            return "https://url";
+        }
+
+
+
+    }
+}
---
+++ ImageResizeWebApp/ImageResizeWebApp/Models/AzureStorageConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageResizeWebApp.Models
+{
+    public class AzureStorageConfig
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+    }
+}
