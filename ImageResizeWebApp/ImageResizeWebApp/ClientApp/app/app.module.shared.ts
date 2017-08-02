ba7cc9be359d828c3a6c6e80f2acd794defc2340
Richard Ortega <richardjortega@gmail.com>
2017-08-02 00:57:12 -0700

    Image resizer JS/HTML test similar to codercards

--- ImageResizeWebApp/ImageResizeWebApp/ClientApp/app/app.module.shared.ts
+++ ImageResizeWebApp/ImageResizeWebApp/ClientApp/app/app.module.shared.ts
@@ -8,13 +8,11 @@ import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
         FetchDataComponent,
         HomeComponent
     ],
@@ -25,11 +23,11 @@ import { CounterComponent } from './components/counter/counter.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
-    ]
+    ],
+    providers: []
 })
 export class AppModuleShared {
 }
