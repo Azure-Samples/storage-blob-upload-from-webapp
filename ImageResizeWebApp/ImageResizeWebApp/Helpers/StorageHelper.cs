05615536151a49b0f16d12499b9c5609035f3bd4
Mark Hopkins <mhopkins@microsoft.com>
2020-01-22 15:46:35 -0800

    Updated to v12

--- ImageResizeWebApp/ImageResizeWebApp/Helpers/StorageHelper.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Helpers/StorageHelper.cs
@@ -1,14 +1,14 @@
 ﻿using ImageResizeWebApp.Models;
 using Microsoft.AspNetCore.Http;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Auth;
-using Microsoft.WindowsAzure.Storage.Blob;
+using Azure.Storage;
+using Azure.Storage.Blobs;
 
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs.Models;
 
 namespace ImageResizeWebApp.Helpers
 {
@@ -29,23 +29,29 @@ namespace ImageResizeWebApp.Helpers
 
         public static async Task<bool> UploadFileToStorage(Stream fileStream, string fileName, AzureStorageConfig _storageConfig)
         {
+            // Create a URI to the blob
+            Uri uri = new Uri("https://" + _storageConfig.AccountName + "/.blob.core.windows.net/" + _storageConfig.ImageContainer + "/" + fileName);
+
             // Create storagecredentials object by reading the values from the configuration (appsettings.json)
-            StorageCredentials storageCredentials = new StorageCredentials(_storageConfig.AccountName, _storageConfig.AccountKey);
+            StorageSharedKeyCredential storageCredentials = new StorageSharedKeyCredential(_storageConfig.AccountName, _storageConfig.AccountKey);
 
             // Create cloudstorage account by passing the storagecredentials
-            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+            //CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+            //BlobServiceClient blobServiceClient = new BlobServiceClient(uri);
 
             // Create the blob client.
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            BlobClient blobClient = new BlobClient(uri, storageCredentials);
 
             // Get reference to the blob container by passing the name by reading the value from the configuration (appsettings.json)
-            CloudBlobContainer container = blobClient.GetContainerReference(_storageConfig.ImageContainer);
+            //CloudBlobContainer container = blobClient.GetContainerReference(_storageConfig.ImageContainer);
+            //BlobContainerClient container = await blobServiceClient.CreateBlobContainerAsync(_storageConfig.ImageContainer);
 
             // Get the reference to the block blob from the container
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+            //CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
 
             // Upload the file
-            await blockBlob.UploadFromStreamAsync(fileStream);
+            //await blockBlob.UploadFromStreamAsync(fileStream);
+            await blobClient.UploadAsync(fileStream);
 
             return await Task.FromResult(true);
         }
@@ -54,40 +60,50 @@ namespace ImageResizeWebApp.Helpers
         {
             List<string> thumbnailUrls = new List<string>();
 
+            // Create a URI to the thumbnail container
+            Uri uri = new Uri("https://" + _storageConfig.AccountName + "/.blob.core.windows.net/");
+
             // Create storagecredentials object by reading the values from the configuration (appsettings.json)
-            StorageCredentials storageCredentials = new StorageCredentials(_storageConfig.AccountName, _storageConfig.AccountKey);
+            //StorageSharedKeyCredential storageCredentials = new StorageSharedKeyCredential(_storageConfig.AccountName, _storageConfig.AccountKey);
 
             // Create cloudstorage account by passing the storagecredentials
-            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+            //CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+            BlobServiceClient blobServiceClient = new BlobServiceClient(uri);
 
             // Create blob client
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            //BlobClient blobClient = new BlobClient(uri, storageCredentials);
 
             // Get reference to the container
-            CloudBlobContainer container = blobClient.GetContainerReference(_storageConfig.ThumbnailContainer);
+            //CloudBlobContainer container = blobClient.GetContainerReference(_storageConfig.ThumbnailContainer);
+            BlobContainerClient container = await blobServiceClient.CreateBlobContainerAsync(_storageConfig.ThumbnailContainer);
 
-            BlobContinuationToken continuationToken = null;
+            //BlobContinuationToken continuationToken = null;
 
-            BlobResultSegment resultSegment = null;
+            //BlobResultSegment resultSegment = null;
 
             //Call ListBlobsSegmentedAsync and enumerate the result segment returned, while the continuation token is non-null.
             //When the continuation token is null, the last page has been returned and execution can exit the loop.
-            do
+            //do
+            //{
+            //    //This overload allows control of the page size. You can return all remaining results by passing null for the maxResults parameter,
+            //    //or by calling a different overload.
+            //    resultSegment = await container.ListBlobsSegmentedAsync("", true, BlobListingDetails.All, 10, continuationToken, null, null);
+
+            //    foreach (var blobItem in resultSegment.Results)
+            //    {
+            //        thumbnailUrls.Add(blobItem.StorageUri.PrimaryUri.ToString());
+            //    }
+
+            //    //Get the continuation token.
+            //    continuationToken = resultSegment.ContinuationToken;
+            //}
+            //while (continuationToken != null);
+
+            foreach (BlobItem blobItem in container.GetBlobs())
             {
-                //This overload allows control of the page size. You can return all remaining results by passing null for the maxResults parameter,
-                //or by calling a different overload.
-                resultSegment = await container.ListBlobsSegmentedAsync("", true, BlobListingDetails.All, 10, continuationToken, null, null);
-
-                foreach (var blobItem in resultSegment.Results)
-                {
-                    thumbnailUrls.Add(blobItem.StorageUri.PrimaryUri.ToString());
-                }
-
-                //Get the continuation token.
-                continuationToken = resultSegment.ContinuationToken;
+                string thumbnailUrl = "https://" + _storageConfig.AccountName + "/.blob.core.windows.net/" + _storageConfig.ThumbnailContainer + "/" + blobItem.Name;
+                thumbnailUrls.Add(thumbnailUrl);
             }
-
-            while (continuationToken != null);
 
             return await Task.FromResult(thumbnailUrls);
         }
